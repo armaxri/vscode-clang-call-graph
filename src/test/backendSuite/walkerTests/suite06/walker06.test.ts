865b3f785e4c0d6dfb341c6fce0cf9b5d4fdb905
arne <armaxri@gmail.com>
2024-03-24 02:08:39 +0100

    Added simple struct tests.

---
+++ src/test/backendSuite/walkerTests/suite06/walker06.test.ts
@@ -0,0 +1,69 @@
+import { testAstWalkerResults } from "../../utils/ast_walker_test";
+
+suite("Clang AST Walker Test Suite 06", () => {
+    test("simple_struct_method implementation", () => {
+        testAstWalkerResults(
+            __dirname,
+            "simple_struct_method.json",
+            [
+                {
+                    funcName: "add",
+                    funcAstName: "__ZN10TestStruct3addEii",
+                    file: "/backendSuite/walkerTests/suite06/simple_struct_method.cpp",
+                    startLoc: {
+                        line: 3,
+                        column: 9,
+                    },
+                    endLoc: {
+                        line: 3,
+                        column: 12,
+                    },
+                },
+                {
+                    funcName: "main",
+                    funcAstName: "_main",
+                    file: "/backendSuite/walkerTests/suite06/simple_struct_method.cpp",
+                    startLoc: {
+                        line: 9,
+                        column: 5,
+                    },
+                    endLoc: {
+                        line: 9,
+                        column: 9,
+                    },
+                },
+            ],
+            undefined,
+            [],
+            []
+        );
+    });
+
+    test("simple_struct_method call", () => {
+        testAstWalkerResults(
+            __dirname,
+            "simple_struct_method.json",
+            undefined,
+            [
+                {
+                    callingFuncAstName: "_main",
+                    callDetails: {
+                        funcName: "add",
+                        funcAstName: "__ZN10TestStruct3addEii",
+                        file: "/backendSuite/walkerTests/suite06/simple_struct_method.cpp",
+                        startLoc: {
+                            line: 12,
+                            column: 12,
+                        },
+                        endLoc: {
+                            line: 12,
+                            column: 26,
+                        },
+                    },
+                },
+            ],
+            [],
+            []
+        );
+    });
+});
