a599311ad248a1b24e11ab94faa69344bcb47cf2
arne <armaxri@gmail.com>
2024-04-25 10:33:12 +0200

    Got a working lowdb dependency and fixed dependency list.

--- src/backend/database/lowdb/LowdbDatabase.ts
+++ src/backend/database/lowdb/LowdbDatabase.ts
@@ -1,5 +1,5 @@
-import { LowSync } from "lowdb";
-import { JSONFileSync } from "lowdb/node";
+import { LowSync } from "@identityinvest/lowdb";
+import { JSONFileSync } from "@identityinvest/lowdb/node";
 // import { JSONFileSyncPreset } from "lowdb/lib/presets/node";
 import { Config } from "../../Config";
 import * as db from "../Database";
