f95587f4660b03ae1ebe932b64ab14055256ab50
arne <armaxri@gmail.com>
2024-06-26 01:09:19 +0200

    Minor database improvement for read access.

--- src/backend/database/Database.ts
+++ src/backend/database/Database.ts
@@ -5,11 +5,13 @@ export { CppFile, Equal, HppFile };
 export interface Database extends Equal {
     getCppFiles(): CppFile[];
     hasCppFile(name: string): boolean;
+    getCppFile(name: string): CppFile | null;
     getOrAddCppFile(name: string): CppFile;
     removeCppFileAndDependingContent(name: string): void;
 
     getHppFiles(): HppFile[];
     hasHppFile(name: string): boolean;
+    getHppFile(name: string): HppFile | null;
     getOrAddHppFile(name: string): HppFile;
     removeHppFileAndDependingContent(name: string): void;
 
