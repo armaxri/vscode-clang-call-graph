9d184ce4dc35dfef3bb2d8b8b914a81b992ec985
arne <armaxri@gmail.com>
2024-07-14 13:34:57 +0300

    More tests for function search functions.

--- src/test/backendSuite/databaseTests/basicFunctionalityTests/getMatchingVirtualFuncImpls/hppFile/hpp_file.suite.ts
+++ src/test/backendSuite/databaseTests/basicFunctionalityTests/getMatchingVirtualFuncImpls/hppFile/hpp_file.suite.ts
@@ -163,4 +163,61 @@ suite("Hpp File", () => {
             });
         }
     );
+
+    suite("Simple with two hpp files and two implementations", () => {
+        [DatabaseType.lowdb, DatabaseType.sqlite].forEach((testData) => {
+            test(`${DatabaseType[testData]}`, () => {
+                const database = openNewDatabase(
+                    __dirname,
+                    testData
+                ) as AbstractDatabase;
+
+                const file1 = database.getOrAddHppFile("file1.h");
+
+                const func1 = file1.addVirtualFuncImpl({
+                    funcName: "func",
+                    funcAstName: "funcBase",
+                    qualType: "int",
+                    range: {
+                        start: { line: 2, column: 2 },
+                        end: { line: 2, column: 10 },
+                    },
+                    baseFuncAstName: "funcBase",
+                });
+
+                const file2 = database.getOrAddHppFile("file2.h");
+
+                const func2 = file2.addVirtualFuncImpl({
+                    funcName: "func",
+                    funcAstName: "funcDerived",
+                    qualType: "int",
+                    range: {
+                        start: { line: 2, column: 2 },
+                        end: { line: 2, column: 10 },
+                    },
+                    baseFuncAstName: "funcBase",
+                });
+
+                database.writeDatabase();
+
+                const funcSearchObject = new FuncSearchObject({
+                    funcName: "func",
+                    baseFuncAstName: "funcBase",
+                    qualType: "int",
+                });
+
+                const foundMatches =
+                    database.getMatchingVirtualFuncImpls(funcSearchObject);
+
+                assert.equal(foundMatches.length, 2);
+                if (foundMatches[0].equals(func1)) {
+                    assert.ok(foundMatches[0].equals(func1));
+                    assert.ok(foundMatches[1].equals(func2));
+                } else {
+                    assert.ok(foundMatches[0].equals(func2));
+                    assert.ok(foundMatches[1].equals(func1));
+                }
+            });
+        });
+    });
 });
