9f7fa8942b45e17ef373daa2d4ba82fa92e105c9
arne <armaxri@gmail.com>
2024-07-16 02:50:26 +0300

    Implemented first tests.

--- src/test/backendSuite/functionSearchTests/cancellationToken/cancellation_token.suite.ts
+++ src/test/backendSuite/functionSearchTests/cancellationToken/cancellation_token.suite.ts
@@ -1,5 +1,17 @@
+import assert from "assert";
+import { CancellationToken } from "../../../../backend/functionSearch/CancellationToken";
 import { addSuitesInSubDirsSuites } from "../../helper/mocha_test_helper";
 
 suite("Cancelation Token", () => {
     addSuitesInSubDirsSuites(__dirname);
+
+    test("Very simple test", () => {
+        const token = new CancellationToken();
+
+        assert.equal(token.isCancelled(), false);
+
+        token.cancel();
+
+        assert.equal(token.isCancelled(), true);
+    });
 });
