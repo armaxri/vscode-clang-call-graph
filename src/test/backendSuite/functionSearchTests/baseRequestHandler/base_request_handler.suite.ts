571a5ed02644d0d786e810833f807a8a2485ac4c
arne <armaxri@gmail.com>
2024-07-16 02:05:16 +0300

    Added test suites to get tests started.

---
+++ src/test/backendSuite/functionSearchTests/baseRequestHandler/base_request_handler.suite.ts
@@ -0,0 +1,5 @@
+import { addSuitesInSubDirsSuites } from "../../helper/mocha_test_helper";
+
+suite("Base Request Handler", () => {
+    addSuitesInSubDirsSuites(__dirname);
+});
---
+++ src/test/backendSuite/functionSearchTests/cancellationToken/cancellation_token.suite.ts
@@ -0,0 +1,5 @@
+import { addSuitesInSubDirsSuites } from "../../helper/mocha_test_helper";
+
+suite("Cancelation Token", () => {
+    addSuitesInSubDirsSuites(__dirname);
+});
