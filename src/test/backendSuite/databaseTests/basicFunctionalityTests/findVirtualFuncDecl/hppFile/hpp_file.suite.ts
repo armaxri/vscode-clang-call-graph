4f6a8689ba66ab024905a6f1332167536ad498a8
arne <armaxri@gmail.com>
2024-07-16 01:11:40 +0300

    Added tests.

---
+++ src/test/backendSuite/databaseTests/basicFunctionalityTests/findVirtualFuncDecl/hppFile/hpp_file.suite.ts
@@ -0,0 +1,168 @@
+import assert from "assert";
+import { DatabaseType } from "../../../../../../backend/Config";
+import { addSuitesInSubDirsSuites } from "../../../../helper/mocha_test_helper";
+import { openNewDatabase } from "../../../typeEqualsTests/database_equality_tests";
+import { AbstractDatabase } from "../../../../../../backend/database/impls/AbstractDatabase";
+import { FuncSearchObject } from "../../../../../../backend/database/FuncSearchObject";
+
+suite("Hpp File", () => {
+    addSuitesInSubDirsSuites(__dirname);
+
+    suite("Decl in class", () => {
+        [DatabaseType.lowdb, DatabaseType.sqlite].forEach((testData) => {
+            test(`${DatabaseType[testData]}`, () => {
+                const database = openNewDatabase(
+                    __dirname,
+                    testData
+                ) as AbstractDatabase;
+
+                const file = database.getOrAddHppFile("file.h");
+                const classInst = file.addClass("class");
+
+                const func = classInst.addVirtualFuncDecl({
+                    funcName: "func",
+                    funcAstName: "func",
+                    baseFuncAstName: "baseFunc",
+                    qualType: "int",
+                    range: {
+                        start: { line: 2, column: 2 },
+                        end: { line: 2, column: 10 },
+                    },
+                });
+
+                database.writeDatabase();
+
+                const funcSearchObject = new FuncSearchObject({
+                    funcName: "func",
+                    funcAstName: "func",
+                    baseFuncAstName: "baseFunc",
+                    qualType: "int",
+                    isVirtual: true,
+                    file: file,
+                });
+
+                const foundMatch = file.findVirtualFuncDecl(funcSearchObject);
+
+                assert.notEqual(foundMatch, null);
+                assert.ok(foundMatch!.equals(func));
+            });
+        });
+    });
+
+    suite("Decl in inner class", () => {
+        [DatabaseType.lowdb, DatabaseType.sqlite].forEach((testData) => {
+            test(`${DatabaseType[testData]}`, () => {
+                const database = openNewDatabase(
+                    __dirname,
+                    testData
+                ) as AbstractDatabase;
+
+                const file = database.getOrAddHppFile("file.h");
+                const outerClassInst = file.addClass("outerClass");
+                const classInst = outerClassInst.addClass("class");
+
+                const func = classInst.addVirtualFuncDecl({
+                    funcName: "func",
+                    funcAstName: "func",
+                    baseFuncAstName: "baseFunc",
+                    qualType: "int",
+                    range: {
+                        start: { line: 2, column: 2 },
+                        end: { line: 2, column: 10 },
+                    },
+                });
+
+                database.writeDatabase();
+
+                const funcSearchObject = new FuncSearchObject({
+                    funcName: "func",
+                    funcAstName: "func",
+                    baseFuncAstName: "baseFunc",
+                    qualType: "int",
+                    isVirtual: true,
+                    file: file,
+                });
+
+                const foundMatch = file.findVirtualFuncDecl(funcSearchObject);
+
+                assert.notEqual(foundMatch, null);
+                assert.ok(foundMatch!.equals(func));
+            });
+        });
+    });
+
+    suite("Decl in header", () => {
+        [DatabaseType.lowdb, DatabaseType.sqlite].forEach((testData) => {
+            test(`${DatabaseType[testData]}`, () => {
+                const database = openNewDatabase(
+                    __dirname,
+                    testData
+                ) as AbstractDatabase;
+
+                const file = database.getOrAddHppFile("file.h");
+                const headerFile = database.getOrAddHppFile("header.h");
+                const classInst = headerFile.addClass("class");
+                headerFile.addReferencedFromFile(file.getName());
+
+                const func = classInst.addVirtualFuncDecl({
+                    funcName: "func",
+                    funcAstName: "func",
+                    baseFuncAstName: "baseFunc",
+                    qualType: "int",
+                    range: {
+                        start: { line: 2, column: 2 },
+                        end: { line: 2, column: 10 },
+                    },
+                });
+
+                database.writeDatabase();
+
+                const funcSearchObject = new FuncSearchObject({
+                    funcName: "func",
+                    funcAstName: "func",
+                    baseFuncAstName: "baseFunc",
+                    qualType: "int",
+                    isVirtual: true,
+                    file: file,
+                });
+
+                const foundMatch = file.findVirtualFuncDecl(funcSearchObject);
+
+                assert.notEqual(foundMatch, null);
+                assert.ok(foundMatch!.equals(func));
+            });
+        });
+    });
+
+    suite("No decl match in inner class or header", () => {
+        [DatabaseType.lowdb, DatabaseType.sqlite].forEach((testData) => {
+            test(`${DatabaseType[testData]}`, () => {
+                const database = openNewDatabase(
+                    __dirname,
+                    testData
+                ) as AbstractDatabase;
+
+                const file = database.getOrAddHppFile("file.h");
+                const headerFile = database.getOrAddHppFile("header.h");
+                const outerClassInst = file.addClass("outerClass");
+                const classInst = outerClassInst.addClass("class");
+                headerFile.addReferencedFromFile(file.getName());
+
+                database.writeDatabase();
+
+                const funcSearchObject = new FuncSearchObject({
+                    funcName: "func",
+                    funcAstName: "func",
+                    baseFuncAstName: "baseFunc",
+                    qualType: "int",
+                    isVirtual: true,
+                    file: file,
+                });
+
+                const foundMatch = file.findVirtualFuncDecl(funcSearchObject);
+
+                assert.equal(foundMatch, null);
+            });
+        });
+    });
+});
