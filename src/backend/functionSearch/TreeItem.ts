e05c3d2a06d38794223b2456e9a868f92d58552a
arne <armaxri@gmail.com>
2024-07-17 16:21:18 +0300

    Implemented getter for outgoing calls.

--- src/backend/functionSearch/TreeItem.ts
+++ src/backend/functionSearch/TreeItem.ts
@@ -1,6 +1,11 @@
 import { Config } from "../Config";
 import { Database } from "../database/Database";
-import { File, FuncBasics } from "../database/cpp_structure";
+import {
+    File,
+    FuncBasics,
+    FuncImplBasics,
+    FuncType,
+} from "../database/cpp_structure";
 import { CancellationToken } from "./CancellationToken";
 
 export class TreeItem {
@@ -38,7 +43,31 @@ export class TreeItem {
     getOutgoingCalls(cancellationToken: CancellationToken): TreeItem[] {
         const outgoingCalls: TreeItem[] = [];
 
-        throw new Error("Method not implemented.");
+        // Only continue if the function is an implementation.
+        if (this.func.getFuncType() === FuncType.implementation) {
+            const funcImpl = this.func as FuncImplBasics;
+
+            // TODO: Consider first ordering the calls by appearance before searching the impls.
+
+            // We get the func calls and map them directly to the corresponding implementations.
+            funcImpl.getFuncCalls().forEach((funcCall) => {
+                const impls = this.database.getFuncImplsOrOneDecl(funcCall);
+                for (const impl of impls) {
+                    outgoingCalls.push(
+                        new TreeItem(this.config, this.database, impl)
+                    );
+                }
+            });
+
+            funcImpl.getVirtualFuncCalls().forEach((funcCall) => {
+                const impls = this.database.getFuncImplsOrOneDecl(funcCall);
+                for (const impl of impls) {
+                    outgoingCalls.push(
+                        new TreeItem(this.config, this.database, impl)
+                    );
+                }
+            });
+        }
 
         return outgoingCalls;
     }
