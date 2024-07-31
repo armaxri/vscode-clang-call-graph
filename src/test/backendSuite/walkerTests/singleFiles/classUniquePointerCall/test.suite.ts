41cf2eebd6e720fc948355f74593328aaf893b8e
arne <armaxri@gmail.com>
2024-07-31 16:56:48 +0200

    Adjusted test suites to run against sqlite as well.
    Former-commit-id: 91b7e84c0424f0b16dc2a8d7672a64bf135eeae7 [formerly 520019acd786074bb638cd5bbeac8dbbda5368dd]
    Former-commit-id: 1121856aa4d4a80773735f708003c5fe523b7cc5

--- src/test/backendSuite/walkerTests/singleFiles/classUniquePointerCall/test.suite.ts
+++ src/test/backendSuite/walkerTests/singleFiles/classUniquePointerCall/test.suite.ts
@@ -1,23 +1,18 @@
 import { DatabaseType } from "../../../../../backend/Config";
 import { addSuitesInSubDirsSuites } from "../../../helper/mocha_test_helper";
-import {
-    testAstWalkerAgainstSpecificDatabase,
-    testAstWalkerResults,
-} from "../../ast_walker_test";
+import { testAstWalkerAgainstSpecificDatabase } from "../../ast_walker_test";
 
 suite("class unique pointer call", () => {
     addSuitesInSubDirsSuites(__dirname);
 
-    test("full test", () => {
-        testAstWalkerResults(__dirname, ["file.json"], "file_expected_db.json");
-    });
-
-    test("test against lowdb", () => {
-        testAstWalkerAgainstSpecificDatabase(
-            __dirname,
-            ["file.json"],
-            "file_expected_db.json",
-            DatabaseType.lowdb
-        );
+    [DatabaseType.lowdb, DatabaseType.sqlite].forEach((testData) => {
+        test(`test against ${DatabaseType[testData]}`, () => {
+            testAstWalkerAgainstSpecificDatabase(
+                __dirname,
+                ["file.json"],
+                "file_expected_db.json",
+                testData
+            );
+        });
     });
 });
