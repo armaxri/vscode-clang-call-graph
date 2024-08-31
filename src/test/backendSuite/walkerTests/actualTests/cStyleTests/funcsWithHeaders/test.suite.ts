9d06832fa6f286435e28bafc9efa8df4c847e964
armaxri <armaxri@gmail.com>
2024-09-01 00:21:43 +0200

    Rearrange walker tests.

---
+++ src/test/backendSuite/walkerTests/actualTests/cStyleTests/funcsWithHeaders/test.suite.ts
@@ -0,0 +1,18 @@
+import { DatabaseType } from "../../../../../../backend/Config";
+import { addSuitesInSubDirsSuites } from "../../../../helper/mocha_test_helper";
+import { testAstWalkerAgainstSpecificDatabase } from "../../../ast_walker_test";
+
+suite("C style functions with headers", () => {
+    addSuitesInSubDirsSuites(__dirname);
+
+    [DatabaseType.lowdb, DatabaseType.sqlite].forEach((testData) => {
+        test(`test against ${DatabaseType[testData]}`, () => {
+            testAstWalkerAgainstSpecificDatabase(
+                __dirname,
+                ["file.json"],
+                "expected_db.json",
+                testData
+            );
+        });
+    });
+});
