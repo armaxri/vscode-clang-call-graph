0ca150f13514d2e6a7205023806aa0cf7f6f79f9
armaxri <armaxri@gmail.com>
2024-09-12 01:13:31 +0200

    Fixed access right.

--- src/extension/VscodeUserInterface.ts
+++ src/extension/VscodeUserInterface.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 import { UserInterface } from "../backend/UserInterface";
 
 export class VscodeUserInterface extends UserInterface {
-    displayError(message: string): void {
+    public displayError(message: string): void {
         vscode.window.showErrorMessage(message);
     }
 }
